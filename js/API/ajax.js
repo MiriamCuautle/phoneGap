e294d68c920eb984e746711de3e010b4374169c9
MiriamCuautle <cansys60@hotmail.com>
2013-03-01 12:40:12 -0600

    http
    http

--- js/API/ajax.js
+++ js/API/ajax.js
@@ -2,8 +2,8 @@
 function registrar(nom, lug, ema, tel){
 	$.ajax({
 			type: "POST",
-			url: "htt://igitsoft.com/pgtest.php",
-			data: "nom="+nom+"&lug="+lug+"&ema="+ema+"&tel="+tel
+			url: "http://igitsoft.com/pgtest.php",
+			data: "nom="+nom+"&lug="+lug+"&mai="+ema+"&tel="+tel
 	}).done(function(msg) {
 		if(msg==1)
 			pgAlert(msg,'Aceptar');
