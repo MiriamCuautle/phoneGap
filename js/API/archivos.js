9054b270b3a42301210f02681f4d760bf28cda74
MiriamCuautle <cansys60@hotmail.com>
2013-03-04 15:09:44 -0600

    r

--- js/API/archivos.js
+++ js/API/archivos.js
@@ -3,14 +3,14 @@
     function uploadFile(ruta) {
         var ft = new FileTransfer();
 
-        ft.upload(ruta.fullPath,
+        ft.upload(ruta,
             "http://igitsoft.com/pgtest.php",
             function(result) {
                 console.log('Upload success: ' + result.responseCode);
                 console.log(result.bytesSent + ' bytes sent');
             },
             function(error) {
-                console.log('Error uploading file ' + path + ': ' + error.code);
+                console.log('Error uploading file ' + ruta + ': ' + error.code);
             },
             { fileKey: "archivo", fileName: name });   
     }
