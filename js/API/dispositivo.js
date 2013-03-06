0eb6af9e95c33d571435d7ad3ed89c6df9dc129f
MiriamCuautle <cansys60@hotmail.com>
2013-03-06 11:45:09 -0600

    s

--- js/API/dispositivo.js
+++ js/API/dispositivo.js
@@ -6,7 +6,7 @@ function dispositivo()
 	dipositivo[name] = device.name;
 	dipositivo[cordova] = device.cordova;
 	dipositivo[platform] = device.platform;
-	dipositivo[uuid] = device.uui;
+	dipositivo[uuid] = device.uuid;
 	dipositivo[version] = device.version;
 	dipositivo[model] = device.model;
 	//Nombre phonegap: device
